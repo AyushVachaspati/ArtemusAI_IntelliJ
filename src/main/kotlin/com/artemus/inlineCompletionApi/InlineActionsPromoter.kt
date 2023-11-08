e8ff70f22bcdfe02cc75ac9df27c6d100f01c091
avach <avachaspati97@gmail.com>
2023-11-09 00:19:42 +0530

    Added TAB to accept suggestions

--- src/main/kotlin/com/artemus/inlineCompletionApi/InlineActionsPromoter.kt
+++ src/main/kotlin/com/artemus/inlineCompletionApi/InlineActionsPromoter.kt
@@ -4,11 +4,19 @@ import com.intellij.openapi.actionSystem.ActionPromoter
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
-import java.util.*
+import java.util.stream.Collectors
 
 class InlineActionsPromoter: ActionPromoter {
-    override fun promote(actions: MutableList<out AnAction>, context: DataContext): MutableList<AnAction>? {
+    override fun promote(actions: List<out AnAction>, context: DataContext): List<AnAction>? {
         val editor = CommonDataKeys.EDITOR.getData(context)
-        return Collections.emptyList()
+        if (editor != null) {
+            val preview = CompletionPreview.getInstance(editor)
+            if (preview != null) {
+                return actions.stream()
+                    .filter { action: AnAction? -> action is InlineCompletionAction }
+                    .collect(Collectors.toList())
+            }
+        }
+        return emptyList()
     }
 }
