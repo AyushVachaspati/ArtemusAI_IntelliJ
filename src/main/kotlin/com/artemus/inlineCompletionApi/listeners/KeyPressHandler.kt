7a6fcb0c230b1e6074adef6a31036f1dadb555fa
avach <avachaspati97@gmail.com>
2023-11-13 23:14:03 +0530

    fixed switching between suggestions on typing

--- src/main/kotlin/com/artemus/inlineCompletionApi/listeners/KeyPressHandler.kt
+++ src/main/kotlin/com/artemus/inlineCompletionApi/listeners/KeyPressHandler.kt
@@ -16,25 +16,24 @@ import com.intellij.psi.PsiFile
 
 class KeyPressHandler: TypedHandlerDelegate() {
     override fun beforeCharTyped(typedChar: Char, project: Project, editor: Editor, file: PsiFile, fileType: FileType): Result {
-
+        println("Char typed: $typedChar")
         // TODO: Check for matching completions. (only if current preview exists) otherwise trigger new completion
         var currentPreview = CompletionPreview.getInstance(editor)
         if(currentPreview!=null) {
             var completions = currentPreview.getCompletions()
             var currentCompletionItem = currentPreview.currentCompletion
-//            var currentIndex = currentPreview.getCurrentIndex()
+            var currentIndex = currentPreview.getCurrentIndex()
 
 
             // filter and adjust the completions which match the typed character
-            completions = completions.filter{
-                it.insertText.startsWith(typedChar)
-            }
-            completions = adjustCompletions(completions, typedChar, currentCompletionItem)
+            completions = filterAndAdjustCompletions(completions, typedChar, currentCompletionItem, currentIndex)
 
             if(completions.isEmpty()){
                 CompletionPreview.clear(editor)
-                // TODO: no completions start with the current types letter
-                // TODO: Trigger new completion here (inside invoke later)
+                val r = Runnable {
+                    ShowTestPreview().createPreview(editor, "On typing new completion")
+                }
+                ApplicationManager.getApplication().invokeLater(r)
                 return Result.CONTINUE
             }
 
@@ -43,41 +42,45 @@ class KeyPressHandler: TypedHandlerDelegate() {
             CompletionPreview.clear(editor)
             GlobalState.clearedByKeyPress = true
             val r = Runnable {
-                GlobalState.clearedByKeyPress = false
                 try {
                     CompletionPreview.createInstance(editor, completions, CompletionType.INLINE_COMPLETION)
                 }
                 catch(e:InvalidDataException){
                     // TODO: Trigger a new completion here
-                    // TODO: Need to work on offset correction in adjustCompletion function to avoid this.
+                    //  Since none of the filtered completions were valid
                     ShowTestPreview().createPreview(editor, "Full text typed trigger")
+                }
+                finally {
+                    GlobalState.clearedByKeyPress = false
                 }
             }
             ApplicationManager.getApplication().invokeLater(r)
         }
         else{
-            // Don't think we need this trigger here
             // TODO: Trigger completion as the user has started typing without any prior completions shown
+            // Don't think we need this trigger here, since Document Listener does that already
         }
 
         return Result.CONTINUE
     }
 
 
-    private fun adjustCompletions(completions: List<InlineCompletionItem>, typedChar: Char,
-                                  currentCompletionItem: InlineCompletionItem
+    private fun filterAndAdjustCompletions(completions: List<InlineCompletionItem>, typedChar: Char,
+                                           currentCompletionItem: InlineCompletionItem, currentIndex: Int
     ): List<InlineCompletionItem> {
         //TODO: Correct offset changes so typing to the end of completions and completions with new line triggers new completion
         //TODO: i.e it does not trigger the InvalidDataException in CompltionPreview constructor.
         val result = ArrayList<InlineCompletionItem>()
-        for (it in completions) {
-            val newInsertText = it.insertText.substring(typedChar.toString().length)
-            if (newInsertText.isNotEmpty()) {
-                if (it.insertText == currentCompletionItem.insertText)
+        for ((i,it) in completions.withIndex()) {
+            if(it.insertText.startsWith(typedChar)) {
+                val newInsertText = it.insertText.substring(typedChar.toString().length)
+                if (newInsertText.isNotEmpty()) {
+                    if (it.insertText == currentCompletionItem.insertText && i == currentIndex)
                     // if the currently shown item matches the prefix. It should continue to be shown
-                    result.add(0, InlineCompletionItem(newInsertText, it.startOffset + 1, it.endOffset + 1) )
-                else {
-                    result.add(InlineCompletionItem(newInsertText, it.startOffset + 1, it.endOffset + 1))
+                        result.add(0, InlineCompletionItem(newInsertText, it.startOffset + 1, it.endOffset + 1))
+                    else {
+                        result.add(InlineCompletionItem(newInsertText, it.startOffset + 1, it.endOffset + 1))
+                    }
                 }
             }
         }
