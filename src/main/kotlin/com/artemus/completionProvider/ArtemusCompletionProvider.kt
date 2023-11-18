38afe58e59c1535f97209c7984cb888354e76823
avach <avachaspati97@gmail.com>
2023-11-18 21:09:08 +0530

    started work on implementing http client

--- src/main/kotlin/com/artemus/completionProvider/ArtemusCompletionProvider.kt
+++ src/main/kotlin/com/artemus/completionProvider/ArtemusCompletionProvider.kt
@@ -6,26 +6,19 @@ import com.intellij.openapi.editor.Editor
 import inference.GRPCInferenceServiceGrpcKt
 import inference.GrpcService
 import io.grpc.ManagedChannelBuilder
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.util.concurrent.TimeUnit
 
 class ArtemusCompletionProvider: InlineCompletionProvider {
     override suspend fun getInlineCompletion(editor: Editor, triggerOffset: Int):List<InlineCompletionItem> {
-
-        val channel = ManagedChannelBuilder.forTarget("127.0.0.1:8001")
-            .idleTimeout(5, TimeUnit.SECONDS)
-            .build();
-
-        val inferenceStub = GRPCInferenceServiceGrpcKt.GRPCInferenceServiceCoroutineStub(channel)
-        val modelLive = inferenceStub.modelReady(
-            GrpcService.ModelReadyRequest.newBuilder()
-                .setName("santacoder_huggingface")
-                .build()
-        )
-        println("#".repeat(100))
-        println(modelLive)
-        println("#".repeat(100))
-        channel.shutdown()
-
+        val client = HttpClient.newHttpClient()
+        val request = HttpRequest.newBuilder(URI.create("http://127.0.0.1:80/v2/models/santacoder_huggingface")).build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        println(response.statusCode())
+        println(response.body())
         return emptyList()
     }
 
@@ -38,3 +31,18 @@ class ArtemusCompletionProvider: InlineCompletionProvider {
         return emptyList()
     }
 }
+
+//val channel = ManagedChannelBuilder.forTarget("127.0.0.1:8001")
+//    .usePlaintext()
+//    .build();
+//
+//val inferenceStub = GRPCInferenceServiceGrpcKt.GRPCInferenceServiceCoroutineStub(channel)
+//val modelLive = inferenceStub.modelReady(
+//    GrpcService.ModelReadyRequest.newBuilder()
+//        .setName("santacoder_huggingface")
+//        .build()
+//)
+//println("#".repeat(100))
+//println(modelLive)
+//println("#".repeat(100))
+//channel.shutdown()
